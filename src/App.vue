623d8471c4050a7675c497353685fd4257f24d1c
GABOGIT79 <contacto@gabosoft.bo>
2024-05-21 22:58:54 -0400

    inicie mi primer repo

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref, computed } from 'vue';
+const name = 'Vue dinámico';
+
+const counter=ref(0);
+const arrayFavoritos = ref([])
+
+const increment = () => counter.value ++;
+const decrement = () => counter.value --;
+const reset = () => (counter.value=0);
+const add = () =>{
+    arrayFavoritos.value.push(counter.value)
+}
+const bloquearAdd = computed(() => {
+    const numSearch = arrayFavoritos.value.find(num => num === counter.value)
+    console.log(numSearch);
+    if(numSearch === 0) return true;
+    return numSearch ? true : false;
+})
+
+const classCounter = computed(() => {
+    if (counter.value === 0) {
+        return 'zero'
+    }
+    if(counter.value > 0){
+        return 'positive'
+    }
+    if(counter.value < 0){
+        return 'negative'
+    }
+})
+</script>
+
+<template>
+    <div class="container text-center mt-3"><h1> Hola {{ name.toUpperCase() }} </h1> <br></br> 
+     <h2 :class="classCounter">{{ counter }}</h2>
+     <div class="btn-group">
+        <button @click="increment" class="btn btn-success">Aumentar</button>
+        <button @click="decrement" class="btn btn-danger">Dismninuir</button>
+        <button @click="reset" class="btn btn-secondary">Reset</button>
+        <button @click="add" :disabled="bloquearAdd" class="btn btn-primary">Add</button>
+     </div>
+     
+     <br></br>
+     {{ arrayFavoritos }}
+     <ul class="list-group mt-4">
+        <li class="list-group-item"
+            v-for="(num, index) in arrayFavoritos"
+            :key="index"
+        >{{ num }}</li>
+
+     </ul></div>
+    
+
+</template>
+
+<style> 
+h1{
+    color: blue;     
+}
+.positive {
+    color: green;
+}
+.negative {
+    color: red;
+}
+.zero {
+    color: peru
+}
+</style>
